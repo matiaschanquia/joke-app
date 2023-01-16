d163dcd3e5b34a369ae72c2ab62b93c238f1a754
matiaschanquia <matiaschanquianahuel@gmail.com>
2023-01-16 20:48:49 -0300

    terminando

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import Footer from "./components/Footer"
 import Header from "./components/Header"
+import Home from "./components/Home"
 
 function App() {
 
   return (
     <div className="container-app">
       <Header />
-      
+      <Home />
       <Footer />
     </div>
   )
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,48 @@
+import { useEffect, useState } from "react"
+
 
 
 const Home = () => {
+    const [input, setInput] = useState("");
+    const [jokes, setJokes] = useState([]);
+    const [loading, setLoading] = useState(false)
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        setJokes([]);
+        setLoading(true);
+        fetch(`https://v2.jokeapi.dev/joke/Programming,Miscellaneous,Dark,Pun,Spooky,Christmas?type=single&contains=${input}&amount=10`)
+            .then(response => response.json())
+            .then(data => {
+                if(data.error) {
+                    alert("There are no jokes with that search");
+                } else {
+                    setJokes(data.jokes);
+                };
+                setLoading(false);
+            })
+            .catch(err => console.log(err));
+    }
+
     return(
         <div className="container-home">
-            
+            <h2 className="titulo">Do a joke search</h2>
+            <form className="form-search" onSubmit={handleSubmit}>
+                <input type="text" onChange={(e) => setInput(e.target.value)} value={input} placeholder="Enter your search..."/>
+                <button type="submit">Search</button>
+            </form>
+            {
+                loading && <p className="loading">Loading...</p>
+            }
+            <ul className="container-jokes">
+                {
+                    jokes.map(item => (
+                        <li key={item.id}>{item.joke}</li>
+                    ))
+                }
+            </ul>
         </div>
     )
 }
+
+export default Home;
